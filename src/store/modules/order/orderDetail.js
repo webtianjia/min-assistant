d562c277bf8ea1817022be07f867ea11b811cce5
chenjiuping@xiuex.com <jiuping520>
2019-05-20 14:56:46 +0800

    修改

--- src/store/modules/order/orderDetail.js
+++ src/store/modules/order/orderDetail.js
@@ -1,7 +1,8 @@
 import order from "@/api/order/order";
 
 const state = {
-  orderDetail: null
+  orderDetail: null,
+  orderRoute: []
 };
 //getters
 const getters = {};
@@ -10,18 +11,31 @@ const getters = {};
 const mutations = {
   setOrderDetail(state, orderDetail) {
     state.orderDetail = orderDetail;
+  },
+  setOrderRoute(state, orderRoute) {
+    state.orderRoute = orderRoute;
   }
 };
 
 // actions
 const actions = {
-  getOrder({ commit }, id) {
-    order.getOrder({ id }).then(response => {
+  getOrder({commit}, id) {
+    order.getOrder({id}).then(response => {
       if (response.success) {
         commit("setOrderDetail", response.data);
       }
     }).catch(error => {
       console.log("获取订单详情失败", error);
+    });
+  },
+
+  async getOrderRoute({commit}, wayBillNo) {
+    await  order.getOrderRoute({wayBillNo}).then(response => {
+      if (response.success) {
+        commit("setOrderRoute", response.data);
+      }
+    }).catch(error => {
+      console.log("获取订单路由失败", error);
     });
   }
 };
